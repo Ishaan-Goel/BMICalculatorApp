ae9dfeaa4f8470969339a01033e74e16cf206853
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2023-12-31 15:33:11 -0800

    Add files via upload

---
+++ BMICalculatorApp/BMICalculator/Models/BMI.swift
@@ -0,0 +1,12 @@
+//
+//  BMI.swift
+//  BMICalculator
+//  Created by Ishaan Goel on 12/25/2023.
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMICalculatorApp/BMICalculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//  Created by Ishaan Goel on 12/25/2023.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
